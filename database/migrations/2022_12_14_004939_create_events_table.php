0e711885748872946d66e90ed01c59a0393ca549
SkalicaSam <samcostehlik@gmail.com>
2022-12-21 15:16:04 +0100

    git init

---
+++ database/migrations/2022_12_14_004939_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('city');
+            $table->integer('city_postcode');
+            $table->string('street');
+            $table->integer('house_number');
+            $table->string('status');
+            $table->string('delivery_text');
+            $table->integer('customer_id');
+            $table->integer('order_id');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
